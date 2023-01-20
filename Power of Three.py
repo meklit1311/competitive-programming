61d744582c3656c5aab784fe8fbbf71373e856b3
meklit1311 <88837746+meklit1311@users.noreply.github.com>
2023-01-20 04:17:20 -0800

    Create Power of Three.py

---
+++ Power of Three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+            if n == 1:
+                return True
+            elif n < 3:
+                return False
+
+            if not n%3:
+                return self.isPowerOfThree(n/3)
+            return False
